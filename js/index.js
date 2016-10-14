27f15f1d2ff353d31907885d39cb55e2aa9ef6e2
Red-Ranger <makamezah@yahoo.com>
2016-10-14 07:39:37 +0000

    first commit to test camera

---
+++ js/index.js
@@ -0,0 +1,41 @@
+
+function setIptions(srcType){
+	var options = {
+		quality:50,
+		destinationType: Camera.DestinationType.FILE_URI,
+		sourceType:srcType,
+		encodingType:Camera.encodingType.JPEG,
+		mediaType: Camera.mediaType.PICTURE,
+		allowEdit:true,
+		correctOrientation:true
+	}
+	return options;
+}
+
+
+function openCamera(){
+	var srcType = Camera.PictureSourceType.CAMERA;
+	var options = setOptions(srcType);
+	var func = createNewFileEntry;
+
+navigator.camera.getPicture(function cameraSuccess(imageUri){
+	displayImage(imageUri);
+		func(imageUri);
+
+},function cameraError(error){
+	console.debug("unable to obtain picture: "+error,"app");
+},options);
+
+var elem = document.getElementById('imageFile');
+elem.src = imageUri;
+}
+
+function onSuccess(imageURL){
+	var image = document.getElelement('pictureTaken');
+	image.src = imageURI;
+}
+
+
+function onFail(message){
+	alert('Failed because:'+message);
+}
