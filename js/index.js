996c92f1e48fe534b9a01108e5dd88b3da726cdb
Red-Ranger <makamezah@yahoo.com>
2016-10-14 13:20:30 +0000

    added barcode scanner

--- js/index.js
+++ js/index.js
@@ -40,3 +40,18 @@ function onSuccess(imageURL){
 function onFail(message){
 	alert('Failed because:'+message);
 }
+
+
+function barcode(){
+	cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+}
